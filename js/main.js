68dfe0e1f8708fa7ac964a09b72b3da522f98859
slimane <slimane@ahah.lan>
2017-07-16 00:14:19 +0200

    Ajourts de la fonction " selection "

--- js/main.js
+++ js/main.js
@@ -1,7 +1,38 @@
-
 // Variables 
 
+// var images = document.getElementsByClassName("slider");
+
 var images = document.getElementsByClassName("slider");
+var turn = 0;
+// FUNCTION
+
+function cacheImages (){ for (i=0 ; i<imgs.length ;i++){
+    imgs[i].style.display="none";
+  } 
+}
+
+function selection (index){
+ imgs[index].style.display="block";
+}
+
+
+ 
+
+// var images = document.getElementsByClassName("slider"); 
+// // FUNCTION
+
+// function cacheImages () {
+// for (i = 0; i < images.length; i++ ) {
+//         imgs[i].style.display ="none";
+// }
+// }
+
+
+
+
+
+
+// // Fonction 
 
 
 
@@ -11,29 +42,9 @@ var images = document.getElementsByClassName("slider");
 
 
 
+// // Script
 
-
-// Fonction 
-function cacheImages (){
-    for (var i = 0; ){
-
-        
-    }
-        
-    }
-
-
-
-
-
-
-
-
-// Script
-
-
-
-
+// cacheImages ();
 
 
 
